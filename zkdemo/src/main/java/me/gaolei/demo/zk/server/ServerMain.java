7c8f23d1bff40367a9baeafa8227ea64c6018bf6
lei <lei@lei-envy.(none)>
2014-03-02 23:08:02 +0800

    update ServerMain.java

--- zkdemo/src/main/java/me/gaolei/demo/zk/server/ServerMain.java
+++ zkdemo/src/main/java/me/gaolei/demo/zk/server/ServerMain.java
@@ -6,11 +6,10 @@ package me.gaolei.demo.zk.server;
 public class ServerMain {
     public static void main(String[] args) throws Exception {
         String hostPost = "127.0.0.1:2181,127.0.0.1:2182,127.0.0.1:2183";
-//        if (args.length < 1) {
-//            System.out.println("Usage: thrift_port");
-//        }
-//        FeatureServer server1 = new FeatureServer(hostPost, Integer.valueOf(args[0]));
-        FeatureServer server1 = new FeatureServer(hostPost, 9092);
+        if (args.length < 1) {
+            System.out.println("Usage: thrift_port");
+        }
+        FeatureServer server1 = new FeatureServer(hostPost, Integer.valueOf(args[0]));
         server1.start();
     }
 }
